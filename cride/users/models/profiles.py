9bfbcf0306b6f36093d69fa7b97cfba4a0d4f6d0
Jesus Herrera <jesusmaherrera@gmail.com>
2019-03-17 00:11:18 -0700

    Clase 12-13

--- cride/users/models/profiles.py
+++ cride/users/models/profiles.py
@@ -13,5 +13,23 @@ class Profile(CRideModel):
 	and statics.
 	"""
 
-	users = models.OneToOneField('users.User', on_delete=models.CASCADE)
+	user = models.OneToOneField('users.User', on_delete=models.CASCADE)
+	picture = models.ImageField(
+		'profile picture',
+		upload_to='users/pictures/',
+		blank=True,
+		null=True,
+	)
+	biography = models.TextField(max_length=500, blank=True)
 
+	# Statics
+	rides_taken = models.PositiveIntegerField(default=0)
+	rides_offered = models.PositiveIntegerField(default=0)
+	reputation = models.FloatField(
+		default=5.0,
+		help_text="User's reputation based on the rides taken and offered."
+	)
+
+	def __str__(self):
+		"""Return users str representarion."""
+		return str(self.user)
