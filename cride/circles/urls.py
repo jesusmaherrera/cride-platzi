87c0f0d6adf7189a940e0ddf5ddf2d53b2603603
Jesus Herrera <jesusmaherrera@gmail.com>
2019-04-03 22:00:10 -0700

    Clase 17

--- cride/circles/urls.py
+++ cride/circles/urls.py
@@ -3,8 +3,9 @@
 from django.urls import path
 
 # Views
-from cride.circles.views import list_circles
+from cride.circles.views import list_circles, create_circlce
 
 urlpatterns = [
-		path('circles/', list_circles)
+		path('circles/', list_circles),
+		path('circles/create/', create_circlce),
 ]
--- cride/circles/views.py
+++ cride/circles/views.py
@@ -1,24 +1,41 @@
 """CIrcles views. """
 
-# Django
-from django.http import HttpResponse, JsonResponse
-
+# Django REST Framework
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 # Models
 from cride.circles.models import Circle
 
-
+@api_view(['GET'])
 def list_circles(request):
-	"""List circles. """
-	circles = Circle.objects.all()
-	public = circles.filter(is_public=True)
-	data = []
-	for circle in circles:
-		data.append({
-			'name': circle.name,
-			'slug_name': circle.slug_name,
-			'rides_offered': circle.rides_offered,
-			'rides_taken': circle.rides_taken,
-			'members_limit': circle.members_limit,
-		})
+    """List circles. """
+    # import ipdb; ipdb.set_trace()
+    circles = Circle.objects.all()
+    public = circles.filter(is_public=True)
+    data = []
+    for circle in circles:
+        data.append({
+            'name': circle.name,
+            'slug_name': circle.slug_name,
+            'rides_offered': circle.rides_offered,
+            'rides_taken': circle.rides_taken,
+            'members_limit': circle.members_limit,
+        })
 
-	return JsonResponse(data, safe=False)
+    return Response(data)
+
+@api_view(['POST'])
+def create_circlce(request):
+    """Create circle."""
+    name = request.data['name']
+    slug_name = request.data['slug_name']
+    about = request.data.get('about', '')
+    circle = Circle.objects.create(name=name, slug_name=slug_name, about=about)
+    data =  {
+        'name': circle.name,
+        'slug_name': circle.slug_name,
+        'rides_offered': circle.rides_offered,
+        'rides_taken': circle.rides_taken,
+        'members_limit': circle.members_limit,
+    }
+    return Response(data)
