8c72fb0087e400e8dd029d67dec61757c906514a
Jesus Herrera <jesusmaherrera@gmail.com>
2019-04-14 09:09:10 -0600

    Clase 24

--- cride/users/serializers/users.py
+++ cride/users/serializers/users.py
@@ -1,53 +1,104 @@
 """User serializers."""
 
 # Django
-from django.contrib.auth import authenticate
+from django.contrib.auth import password_validation, authenticate
+from django.core.validators import RegexValidator
 
 # Django REST Framework
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
+from rest_framework.validators import UniqueValidator
 
 # Models
-from cride.users.models import User
+from cride.users.models import User, Profile
+
 
 class UserModelSerializer(serializers.ModelSerializer):
-	"""User model serialzier."""
+    """User model serialzier."""
 
-	class Meta:
-		"""Meta class."""
-		model = User
-		fields = (
-			'username',
-			'first_name',
-			'last_name',
-			'email',
-			'phone_number',
-		)
+    class Meta:
+        """Meta class."""
+        model = User
+        fields = (
+            'username',
+            'first_name',
+            'last_name',
+            'email',
+            'phone_number',
+        )
 
+
+class UserSignUpSerializer(serializers.Serializer):
+    """User signup serializer.
+
+    Handle signup data validation and user/profile creation.
+    """
+
+    email = serializers.EmailField(
+        validators=[
+            UniqueValidator(queryset=User.objects.all())
+        ]
+    )
+
+    username = serializers.CharField(
+        min_length=4,
+        max_length=20,
+        validators=[
+            UniqueValidator(queryset=User.objects.all())
+        ],
+    )
+
+    # Phone number
+    phone_regex = RegexValidator(
+        regex=r'\+?1?\d{9,15}$',
+        message='Phone number must be entered in the format +99999999.'
+                ' Up to 15 digits allowed.'
+    )
+    phone_number = serializers.CharField(
+        validators=[phone_regex, ],
+        max_length=17,
+    )
+    password = serializers.CharField(min_length=8, max_length=64)
+    password_confirmation = serializers.CharField(min_length=8, max_length=64)
+
+    first_name = serializers.CharField(min_length=1, max_length=30)
+    last_name = serializers.CharField(min_length=1, max_length=30)
+
+    def validate(self, data):
+        """Verifiy passwords match."""
+        passwd = data['password']
+        passwd_conf = data['password_confirmation']
+        if passwd != passwd_conf:
+            raise serializers.ValidationError("Password don't match.")
+        password_validation.validate_password(passwd)
+        return data
+    def create(self, data):
+        """Handle user and profile creation."""
+        data.pop("password_confirmation")
+        user = User.objects.create_user(**data)
+        profile = Profile.objects.create(user=user)
+        return user, profile
 
 
 class UserLoginSerializer(serializers.Serializer):
-	"""User login serializer.
+    """User login serializer.
 
-	Handle the login request data.
-	"""
+    Handle the login request data.
+    """
 
-	email = serializers.EmailField()
-	password = serializers.CharField(min_length=8, max_length=64)
+    email = serializers.EmailField()
+    password = serializers.CharField(min_length=8, max_length=64)
 
-	def validate(self, data):
-		"""Check credientials."""
-		user = authenticate(username=data['email'], password=data['password'])
-		if not user:
-			raise serializers.ValidationError('Invalid credientials')
-		self.context['user'] = user
-		return data
+    def validate(self, data):
+        """Check credientials."""
+        user = authenticate(username=data['email'], password=data['password'])
+        if not user:
+            raise serializers.ValidationError('Invalid credientials')
+        self.context['user'] = user
+        return data
 
-	def create(self, data):
-		"""Generate or retrieve new token."""
+    def create(self, data):
+        """Generate or retrieve new token."""
 
-		token, created = Token.objects.get_or_create(user=self.context['user'])
-		return self.context['user'], token.key
-
-
-
+        token, created = Token.objects.get_or_create(user=self.context['user'])
+        return self.context['user'], token.key
--- cride/users/urls.py
+++ cride/users/urls.py
@@ -3,8 +3,9 @@
 from django.urls import path
 
 # Views
-from cride.users.views import UserLoginAPIView
+from cride.users.views import UserLoginAPIView, SignUpAPIView
 
 urlpatterns = [
 		path('users/login/', UserLoginAPIView.as_view(), name='login'),	
+		path('users/signup/', SignUpAPIView.as_view(), name='signup'),	
 ]
