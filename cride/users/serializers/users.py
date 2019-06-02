c9fd3f8989310cff4688deb528dc1ff9a8c4356f
desarrollo_copamex <jesus.herrera@copamex.com>
2019-06-01 18:54:42 -0600

    27 - Instalar PyJWT y generar tokens

--- cride/users/serializers/users.py
+++ cride/users/serializers/users.py
@@ -1,10 +1,12 @@
 """User serializers."""
 
 # Django
+from django.conf import settings
 from django.contrib.auth import password_validation, authenticate
 from django.core.validators import RegexValidator
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
+from django.utils import timezone
 
 # Django REST Framework
 from rest_framework import serializers
@@ -13,6 +15,10 @@ from rest_framework.validators import UniqueValidator
 
 # Models
 from cride.users.models import User, Profile
+
+# Utilities
+from datetime import timedelta
+import jwt
 
 
 class UserModelSerializer(serializers.ModelSerializer):
@@ -99,7 +105,15 @@ class UserSignUpSerializer(serializers.Serializer):
 
     def get_verification_token(self, user):
         """Create JWT token that the user can use to verify its account."""
-        return 'abc'
+        exp_date = timezone.now() + timedelta(seconds=1)
+        payload = {
+            'user': user.username,
+            'exp': int(exp_date.timestamp()),
+            'type': 'email_confirmation'
+        }
+        token = jwt.encode(payload, settings.SECRET_KEY, algorithm='HS256')
+        return token.decode()
+
 
 
 class UserLoginSerializer(serializers.Serializer):
