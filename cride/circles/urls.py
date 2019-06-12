db102acc8dc95fc998155adfa7db5c7b6e7b8df4
desarrollo_copamex <jesus.herrera@copamex.com>
2019-06-12 16:02:39 -0600

    30 - Crear CircleViewSet

--- cride/circles/urls.py
+++ cride/circles/urls.py
@@ -1,11 +1,17 @@
 """Circle URLs.."""
+
 # Django
-from django.urls import path
+from django.urls import include, path
+
+# Django REST Framework
+from rest_framework.routers import DefaultRouter
 
 # Views
-from cride.circles.views import list_circles, create_circlce
+from .views import circles as circle_views
+
+router = DefaultRouter()
+router.register(r'circles', circle_views.CircleViewSet, basename='circle')
 
 urlpatterns = [
-		path('circles/', list_circles),
-		path('circles/create/', create_circlce),
+	path('', include(router.urls))
 ]
