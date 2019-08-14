a83903a6d0dbebcb96335c471d5dd4027fc87b0f
desarrollo_copamex <jesus.herrera@copamex.com>
2019-08-14 17:39:56 -0600

    34 - Detalle de usuario

--- cride/users/views/users.py
+++ cride/users/views/users.py
@@ -6,7 +6,15 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+# Permissions
+from rest_framework.permissions import (
+	AllowAny,
+	IsAuthenticated
+)
+from cride.users.permissions import IsAccountOwner
+
 # Serializers
+from cride.circles.serializers import CircleModelSerializer
 from cride.users.serializers import (
 	AcountVerificationSerializer,
 	UserLoginSerializer,
@@ -14,11 +22,35 @@ from cride.users.serializers import (
 	UserSignUpSerializer
 )
 
-class UserViewSet(viewsets.GenericViewSet):
+# Models
+from cride.users.models import User
+from cride.circles.models import Circle
+
+membership__is_active=True
+
+class UserViewSet(mixins.RetrieveModelMixin,
+				  viewsets.GenericViewSet):
 	"""User view set.
 	Handle sign up, login and account verifications
 	"""
-	
+
+	queryset = User.objects.filter(is_active=True, is_client=True)
+	serializer_class = UserModelSerializer
+	lookup_field = 'username' 
+
+
+	def get_permissions(self):
+		"""Assign permissions based on action."""
+		if self.action in ['signup', 'login', 'verify']:
+			permissions = [AllowAny]
+		elif self.action == 'retrive':
+			permissions = [IsAuthenticated, IsAccountOwner]
+		else:
+			permissions = [IsAuthenticated]
+
+		return [permission() for permission in permissions]
+
+  
 	@action(detail=False, methods=['post'])
 	def signup(self, request):
 		"""User Sign up."""
@@ -41,6 +73,21 @@ class UserViewSet(viewsets.GenericViewSet):
 			'access_token': token,
 		}
 		return Response(data, status=status.HTTP_201_CREATED)
+
+	def retrieve(self, request, *args, **kwargs):
+		"""Add extra data to the response."""
+		response = super().retrieve(request, *args, **kwargs)
+		circles = Circle.objects.filter(
+			members=request.user,
+			membership__is_active=True
+
+		)
+		data ={
+			'user': response.data,
+			'circles': CircleModelSerializer(circles, many=True).data
+		} 
+		response.data = data
+		return response
 
 
 	@action(detail=False, methods=['post'])
