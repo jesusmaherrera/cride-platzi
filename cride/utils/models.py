bd5338f5fc11606440057977458c265e1e0c939e
Jesus Manuel Herrera <jesusmaherrera@gmail.com>
2020-01-02 16:25:18 -0700

    clase 3-1

---
+++ cride/utils/models.py
@@ -0,0 +1,33 @@
+"""Django mdoels utilities."""
+
+# django
+from django.db import models
+
+
+class CRideModel(models.Model):
+    """Comparte Ride base model.
+
+    CrideModel acts as an abstract base class from witchnevery
+    other model in the project will inherit. This class provides
+    every table with de following attribute:
+        + created (DateTime): Store the datetime the object was created
+        + modified (DateTime): Store the last datetime the object was modified
+    """
+
+    created = models.DateTimeField(
+        'created_at',
+        auto_now_add=True,
+        help_text="Date time on with the object was created."
+    )
+    modified = models.DateTimeField(
+        'modified_at',
+        auto_now=True,
+        help_text="Date time on with the object was last modified."
+    )
+
+    class Meta(object):
+        """docstring for Meta"""
+        abstract = True
+
+        get_lates_by = 'created'
+        ordering = ('-created', '-modified')
