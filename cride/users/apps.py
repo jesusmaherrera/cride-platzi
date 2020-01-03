84e674125212badd45ea4fe3c52a790e1c28e4b2
Jesus Manuel Herrera <jesusmaherrera@gmail.com>
2020-01-02 19:36:24 -0700

    Clase 3 App de usuarios

---
+++ cride/users/apps.py
@@ -0,0 +1,10 @@
+"""Users app."""
+
+# django
+from django.apps import AppConfig
+
+
+class UsersAppConfig(AppConfig):
+    """Users app Config."""
+    name = 'cride.users'
+    verbose_name = 'Users'
