85041f474c2b15da6b2d3802f3ef579f4ba62a7d
Jesus Herrera <jesusmaherrera@gmail.com>
2020-01-25 23:38:11 -0700

    Clase 4- Vistas, URLS, Paresers de DRF

--- cride/circles/urls.py
+++ cride/circles/urls.py
@@ -4,9 +4,10 @@
 from django.urls import path
 
 # views
-from cride.circles.views import list_circles, import_circles
+from cride.circles.views import list_circles, import_circles, create_circle
 
 urlpatterns = [
     path('circles/', list_circles),
     path('circles/import/', import_circles),
+    path('circles/create/', create_circle),
 ]
--- cride/circles/views.py
+++ cride/circles/views.py
@@ -3,18 +3,19 @@
 # python
 import csv
 
-# Django
-from django.http import JsonResponse
+# Django REST Framework
+from rest_framework.decorators import api_view 
+from rest_framework.response import Response
 
 # models
 from cride.circles.models import Circle
 
+@api_view(['GET'])
 def list_circles(request):
 	"""List circles."""
-	circles =  Circle.objects.all()
-	public = circles.filter(is_public=True)
+	circles =  Circle.objects.filter(is_public=True)
 	data = []
-	for circle in public:
+	for circle in circles:
 		data.append({
 			'name': circle.name,
 			'slug_name': circle.slug_name,
@@ -23,7 +24,29 @@ def list_circles(request):
 			'is_limited': circle.is_limited,
 			'members_limit': circle.members_limit,
 		})
-	return JsonResponse(data, safe=False)
+	return Response(data)
+
+@api_view(['POST'])
+def create_circle(request):
+	"""Create Circle."""
+	name =  request.data['name']
+	slug_name =  request.data['slug_name']
+	about =  request.data.get('about','')
+	circle = Circle.objects.create(
+		name=name,
+		slug_name=slug_name,
+		about=about,
+	)
+	data = {
+		'name': circle.name,
+		'slug_name': circle.slug_name,
+		'is_public': circle.is_public,
+		'verified': circle.is_verified,
+		'is_limited': circle.is_limited,
+		'members_limit': circle.members_limit,
+	}
+	return Response(data)
+
 
 def import_circles(request):
 	"""import circles from a csv file."""
