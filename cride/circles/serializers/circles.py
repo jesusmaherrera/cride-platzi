900ae59f4404373ce98f4379ae75947f84bc840d
Jesus Manuel Herrera <jesusmaherrera@gmail.com>
2020-04-18 12:44:40 -0600

    Clase 5 - Creación de circulos

--- cride/circles/serializers/circles.py
+++ cride/circles/serializers/circles.py
@@ -10,6 +10,20 @@ from cride.circles.models import Circle
 class CircleModelSerializer(serializers.ModelSerializer):
     """Circle model serializer."""
 
+    members_limit = serializers.IntegerField(
+        required=False,
+        min_value=10,
+        max_value=32000,
+    )
+    is_limited = serializers.BooleanField(default=False)
+
+    read_only_fields = (
+        'is_public',
+        'verified',
+        'rides_offered',
+        'rides_taken',
+    )
+
     class Meta:
         """Meta Class."""
         model = Circle
@@ -20,3 +34,11 @@ class CircleModelSerializer(serializers.ModelSerializer):
             'is_verified', 'is_public',
             'is_limited', 'members_limit'
         )
+
+    def validate(self, data):
+        """Ensure both members_limit and is_limited are present."""
+        members_limit = data.get('members_limit', None)
+        is_limited = data.get('is_limited', False)
+        if is_limited ^ bool(members_limit):
+            raise serializers.ValidationError('If cirlce is limited, a member limit must be provided.')
+        return data
--- cride/circles/views/circles.py
+++ cride/circles/views/circles.py
@@ -4,7 +4,7 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 # Models
-from cride.circles.models import Circle
+from cride.circles.models import Circle, Membership
 
 # Serializers
 from cride.circles.serializers import CircleModelSerializer
@@ -19,6 +19,19 @@ class CircleViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         """Restrict list to public-only."""
         queryset = Circle.objects.all()
-        # if self.action == 'list':
-        #     return queryset.filter(is_public=True)
+        if self.action == 'list':
+            return queryset.filter(is_public=True)
         return queryset
+
+    def perform_create(self, serializer):
+        """Assign circle admin."""
+        circle = serializer.save()
+        user = self.request.user
+        profile = user.profile
+        Membership.objects.create(
+            user=user,
+            profile=profile,
+            circle=circle,
+            is_admin=True,
+            remaining_invitations=10,
+        )
