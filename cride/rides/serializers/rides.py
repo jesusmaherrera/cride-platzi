be1cec7a89ab29b6262f6c2054ab6844570bd428
Jesus Manuel Herrera <jesusmaherrera@gmail.com>
2020-04-21 16:25:47 -0600

    Editar un ride

--- cride/rides/serializers/rides.py
+++ cride/rides/serializers/rides.py
@@ -7,6 +7,9 @@ from rest_framework import serializers
 from cride.circles.models import Membership
 from cride.rides.models import Ride
 
+# Serializers
+from cride.users.serializers import UserModelSerializer
+
 # Utilities
 from datetime import timedelta
 from django.utils import timezone
@@ -14,6 +17,11 @@ from django.utils import timezone
 
 class RideModelSerializer(serializers.ModelSerializer):
     """Ride model serializer."""
+    offered_by = UserModelSerializer(read_only=True)
+    offered_in = serializers.StringRelatedField()
+
+    passangers = UserModelSerializer(read_only=True, many=True)
+
     class Meta:
         """Meta class."""
 
@@ -25,6 +33,13 @@ class RideModelSerializer(serializers.ModelSerializer):
             'rating',
             'is_active',
         )
+
+    def update(self, instance, validated_data):
+        """Allow updates only before departure date."""
+        now = timezone.now()
+        if instance.departure_date <= now:
+            raise serializers.ValidationError('Ongoing rides cannot be modified.')
+        return super().update(instance, validated_data)
 
 
 class CreateRideSerializer(serializers.ModelSerializer):
--- cride/rides/views/rides.py
+++ cride/rides/views/rides.py
@@ -7,6 +7,7 @@ from rest_framework.generics import get_object_or_404
 # Permisions
 from rest_framework.permissions import IsAuthenticated
 from cride.circles.permissions.memberships import IsActiveCircleMember
+from cride.rides.permissions.rides import IsRideOwner
 
 # Filters
 from rest_framework.filters import SearchFilter, OrderingFilter
@@ -27,14 +28,27 @@ from django.utils import timezone
 
 class RideViewSet(mixins.ListModelMixin,
                   mixins.CreateModelMixin,
+                  mixins.UpdateModelMixin,
                   viewsets.GenericViewSet):
     """Ride view set."""
 
-    permission_classes = [IsAuthenticated, IsActiveCircleMember, ]
     filter_backends = (SearchFilter, OrderingFilter, )
     ordering = ('departure_date', 'arrival_date', 'available_seats')
     ordering_fields = ('departure_date', 'arrival_date', 'available_seats')
     search_fields = ('departure_location', 'arrival_location')
+
+    def dispatch(self, request, *args, **kwargs):
+        """Verify that the cirle exists."""
+        slug_name = kwargs['slug_name']
+        self.circle = get_object_or_404(Circle, slug_name=slug_name)
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_permissions(self):
+        """Assign permissions based on action."""
+        permissions = [IsAuthenticated, IsActiveCircleMember]
+        if self.action in ['update', 'partial_update', ]:
+            permissions.append(IsRideOwner)
+        return [permission() for permission in permissions]
 
     def get_serializer_class(self):
         """Return serializer based on action."""
@@ -42,12 +56,6 @@ class RideViewSet(mixins.ListModelMixin,
             return CreateRideSerializer
 
         return RideModelSerializer
-
-    def dispatch(self, request, *args, **kwargs):
-        """Verify that the cirle exists."""
-        slug_name = kwargs['slug_name']
-        self.circle = get_object_or_404(Circle, slug_name=slug_name)
-        return super().dispatch(request, *args, **kwargs)
 
     def get_serializer_context(self):
         """Add cricle to serializer context.
