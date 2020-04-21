bc3f246056fce9f3d8891294c8de61fc0ef0d55e
Jesus Manuel Herrera <jesusmaherrera@gmail.com>
2020-04-21 10:06:56 -0600

    Clase 5 - App de rides y modelos

---
+++ cride/rides/apps.py
@@ -0,0 +1,10 @@
+"""Rides app."""
+
+# django
+from django.apps import AppConfig
+
+
+class RidesAppConfig(AppConfig):
+    """Rides app Config."""
+    name = 'cride.rides'
+    verbose_name = 'Rides'
