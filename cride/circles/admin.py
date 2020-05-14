d2000327d6597b333fb752c6e5ce6222e25e1c35
Jesus Herrera <jesusmaherrera@gmail.com>
2020-05-14 07:10:16 -0600

    Clase 8 - Admin actions: Regresando una respuesta HTTP

--- cride/circles/admin.py
+++ cride/circles/admin.py
@@ -2,9 +2,16 @@
 
 # Django
 from django.contrib import admin
+from django.http import HttpResponse
 
 # models
 from cride.circles.models import Circle
+from cride.rides.models import Ride
+
+# Utilities
+from django.utils import timezone
+from datetime import datetime, timedelta
+import csv
 
 
 @admin.register(Circle)
@@ -25,7 +32,7 @@ class CirlceAdmin(admin.ModelAdmin):
         'is_verified',
         'is_limited',
     )
-    actions = ['make_verified', 'make_unverified']
+    actions = ['make_verified', 'make_unverified', 'download_todays_rides']
 
     def make_verified(self, request, queryset):
         """make circles verified."""
@@ -36,3 +43,31 @@ class CirlceAdmin(admin.ModelAdmin):
         """make circles unverified."""
         queryset.update(is_verified=False)
     make_unverified.short_description = 'Make selected Circle unverified'
+
+    def download_todays_rides(self, request, queryset):
+        """Return today's rides."""
+        now = timezone.now()
+        start = datetime(now.year, now.month, now.day, 0, 0, 0)
+        end = start + timedelta(days=1)
+        rides = Ride.objects.filter(
+            offered_in__in=queryset.values_list('id'),
+            departure_date__gte=start,
+            departure_date__lte=end,
+        ).order_by('departure_date')
+        response = HttpResponse(content_type='text/csv')
+        response[
+            'Content-Disposition'] = 'attachment; filename="somefilename.csv"'
+        writer = csv.writer(response)
+        writer.writerow([
+            'id',
+            'passengers',
+            'departure_location',
+        ])
+        for ride in rides:
+            writer.writerow([
+                ride.pk,
+                ride.passengers.count(),
+                ride.departure_location
+            ])
+        return response
+    download_todays_rides.short_description = "Download today's rides"
